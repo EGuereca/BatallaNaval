372120220f6e11c267ff552f159e49d88e641a5f
IKER1208 <iker34267@gmail.com>
2025-06-16 10:47:22 -0600

    Refactor foreign key constraints in games table migration to explicitly reference 'id' on 'users' table

--- database/migrations/2025_06_16_010021_create_games_table.php
+++ database/migrations/2025_06_16_010021_create_games_table.php
@@ -16,11 +16,13 @@ return new class extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('player1_id')->constrained()->onDelete('cascade');
-            $table->foreignId('player2_id')->constrained()->onDelete('cascade');
+            $table->foreignId('player1_id')->constrained()->references('id')->on('users')-> onDelete('cascade');
+            $table->foreignId('player2_id')->constrained()->references('id')->on('users')-> onDelete('cascade');
             $table->enum ('status', ['waiting', 'playing', 'finished'])->default('waiting');
-            $table->foreignId('winner_id')->nullable()->constrained('users')->onDelete('set null');
-            $table->foreignId('current_turn_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('winner_id')->nullable()->constrained('users')->references('id')->onDelete('set null');
+            $table->foreignId('current_turn_id')->nullable()->constrained('users')->references('id')->onDelete('set null');
+
+
         });
     }
 
