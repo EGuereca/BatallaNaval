b0f68228d8ad862e97da256d94dd35513ba9a987
IKER1208 <iker34267@gmail.com>
2025-06-17 10:05:18 -0600

    sobres

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GameController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +34,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rutas de juegos (migradas desde api.php)
+    Route::get('/games', [GameController::class, 'index'])->name('games.index');
+    Route::get('/games/{game}', [GameController::class, 'show'])->name('games.show');
+    Route::post('/games', [GameController::class, 'createGame'])->name('games.create');
+    Route::post('/games/{game}/join', [GameController::class, 'joinGame'])->name('games.join');
+    Route::post('/games/{game}/move', [GameController::class, 'makeMove'])->name('games.move');
+    Route::get('/games/{game}/opponent-board', [GameController::class, 'getOpponentBoard'])->name('games.opponent-board');
+    Route::get('/games/{game}/board/{playerNumber}', [GameController::class, 'getPlayerBoard'])->name('games.player-board');
 });
 
 require __DIR__.'/auth.php';
